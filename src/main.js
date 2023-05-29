ffc2a0e08edde085445a31eb0c71b7d051bf184f
Todd Whitelow <todd@whitelow.tech>
2023-05-29 00:29:12 -0600

    change axios url

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue';
 import router from './router';
 import axios from "axios";
 
-axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:1337/api/" : "https://portfoliovtwo.herokuapp.com/";
+axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "https://portfoliovtwo.herokuapp.com/" : "/";
 
 Vue.config.productionTip = false;
 Vue.prototype.$apiHostname = process.env.NODE_ENV === 'development' ? 'http://localhost:1337' : '/';
