fb780788123acb4c6ac5b0b5d1b268e4edf2eaa9
Todd Whitelow <todd@whitelow.tech>
2023-06-05 00:34:06 -0600

    update blog page

--- src/views/BlogsIndex.vue
+++ src/views/BlogsIndex.vue
@@ -2,15 +2,15 @@
   <div class="project">
     <section class="cta-section theme-bg-light py-5">
 		    <div class="container text-center">
-			    <h2 class="heading">A Blog About Software Development And Life</h2>
-			    <div class="intro">Welcome to my blog. Subscribe and get my latest blog post in your inbox.</div>
-			    <form class="signup-form form-inline justify-content-center pt-3">
-                    <div class="form-group">
+			    <h2 class="heading">A Blog About My Journey In Software Engineering</h2>
+			    <!-- <div class="intro">Welcome to my blog. Subscribe and get my latest blog post in your inbox.</div> -->
+			    <!-- <form class="signup-form form-inline justify-content-center pt-3"> -->
+                    <!-- <div class="form-group">
                         <label class="sr-only" for="semail">Your email</label>
                         <input type="email" id="semail" name="semail1" class="form-control mr-md-1 semail" placeholder="Enter email">
                     </div>
                     <button type="submit" class="btn btn-primary">Subscribe</button>
-                </form>
+                </form> -->
 		    </div><!--//container-->
 	    </section>
 	    <section class="blog-list px-3 py-5 p-md-5">
@@ -18,7 +18,7 @@
 			    <div class="row">
 					<div class="col-md-4 mb-3" v-for="blog in blogs.data">
 						<div class="card blog-post-card">
-							<img class="card-img-top" v-bind:src="blog.attributes.previewPhoto" alt="image" />
+							<!-- <img class="card-img-top" v-bind:src="blog.attributes.previewPhoto" alt="image" /> -->
 							<div class="card-body">
 								<h5 class="card-title"><a class="theme-link" href="blog-post.html">{{ blog.attributes.title }}</a></h5>
 								<p class="card-text" v-html="blog.attributes.snippet"></p>
